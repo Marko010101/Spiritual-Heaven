8399d82344840dc945c14ce4dd0c563e891f3243
Marko <baghashvili.marko@gmail.com>
2024-04-05 23:05:17 +0400

    Created reusable pagination and implemented pagination for the bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -6,21 +6,30 @@ export function useBookings() {
   const [searchParams] = useSearchParams();
 
   // FILTER
-
   const filterValue = searchParams.get("status");
   const filter =
     !filterValue || filterValue === "all"
       ? null
       : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
 
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  // PAGINATION
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+  // QUERY
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, bookings, error };
+  return { isLoading, error, bookings, count };
 }
